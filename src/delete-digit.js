18a1f054f3cf9b41c8f36f160bceaf3115f65cc2
Elena Kanash <hird250@gmail.com>
2022-10-16 16:52:29 +0300

    feat: task complete

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,10 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  //throw new NotImplementedError('Not implemented');
+  const number = n.toString();
+  const result = [];
+
+  for (let i = 0; i < number.length; i++) {
+    result.push(number.slice(0, i) + number.slice(i + 1))
+    console.log(i, number.slice(0,  i));
+    console.log(i, number.slice(i + 1));
+    console.log('result: ', result);
+  }
+  result.map(item => +item);
+  let max =  Math.max(...result);
+  return max;
 }
+deleteDigit(152)
 
 module.exports = {
   deleteDigit
